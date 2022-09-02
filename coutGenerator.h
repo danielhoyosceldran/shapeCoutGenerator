adfae10a59383ac8451bf4a4222712897849496f
Daniel <danhoycel@gmail.com>
2022-09-02 18:41:25 +0200

    Working

---
+++ coutGenerator.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+class coutGenerator
+{
+private:
+    /* data */
+public:
+    //coutGenerator(/* args */);
+    //~coutGenerator();
+    void generarTop(ofstream &, char *, int);
+    bool generarFitxer(int, int, int);
+};
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "coutGenerator.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Generador de 'cout' per a dibuixar figures.\nAmplada de la figura: ";
+    int alcada, amplada;
+    cin >> amplada;
+
+    cout << "Alcada: ";
+    cin >> alcada;
+
+    cout << "Quantes figures vols per fila? ";
+    int quantitat;
+    cin >> quantitat;
+    // Control d'error
+    if (quantitat < 0)
+    {
+        cout << " [e] -> " << quantitat << " no és una quantitat vàlida" << endl;
+        return -1;
+    }
+    cout << "[i] -> Generant fitxer..." << endl;
+
+
+    coutGenerator coutGen;
+
+    if (!coutGen.generarFitxer(amplada, alcada, 1)) // Treballant en el numero de figures que podem fer en paral·lel
+    {
+        cout << " [e] -> No s'ha pogut generar el fitxer" << endl;
+        return -1;
+    }
+    cout << "[i] -> El fitxer s'ha generat correctment" << endl;
+
+    return 0;
+}
